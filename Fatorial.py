72a68aca37378169634cac475d934084e8ff8e3f
LeandroJoseph <167385603+LeandroJoseph@users.noreply.github.com>
2026-02-24 10:35:41 -0300

    Delete Fatorial.py

--- Fatorial.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def executar_fat():
-    n1=int(input("Digite um valor:\n"))
-
-    resultado = math.factorial(n1)
-
-    print(f"O fatorial de {n1} é {resultado}")
-    return resultado
